debaf50003e0a368e7705c3690c5f8a4db06566c
wangfan13 <wangfan13@kuaishou.com>
2026-07-21 20:40:15 +0800

    feat: 精美积时图标 + 完整 iOS apple-touch-icon 配置

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,11 @@ export default defineConfig({
         scope: '/jishi/',
         icons: [
           {
+            src: 'icons/icon-180.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+          {
             src: 'icons/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
